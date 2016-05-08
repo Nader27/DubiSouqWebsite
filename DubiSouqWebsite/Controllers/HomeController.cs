bfaef8d0dfba22fbd1ce0deb396104cd773d08f8
Nader Alaa El-Din <nader_alaa@hotmail.com>
2016-05-08 04:19:19 +0200

    Nader

--- DubiSouqWebsite/Controllers/HomeController.cs
+++ DubiSouqWebsite/Controllers/HomeController.cs
@@ -1,16 +1,48 @@
-﻿using System;
-using DubiSouqWebsite.Models;
+﻿using DubiSouqWebsite.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.IO;
 
 namespace DubiSouqWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        private Entities db = new Entities();
+
         public ActionResult Home()
         {
+            List<product> products = db.products.Where(p => p.Type_ID == 1).ToList();
+            ViewBag.saleproduct = products.OrderByDescending(p => p.Sale).Take(10).ToList();
+            ViewBag.newproduct = products.OrderByDescending(p => p.Post_Time).Take(10).ToList();
+            ViewBag.cheapproduct = products.OrderBy(p => p.Price).Take(10).ToList();
+            List <KeyValuePair< product, int>> Dict = new List<KeyValuePair<product, int>>();
+            foreach (product product in products)
+            {
+                int count = 0;
+                int rate = 0;
+                List<review> reviews = db.reviews.Where(r => r.Product_ID == product.ID).ToList();
+                foreach (review review in reviews)
+                {
+                    rate += review.Rate;
+                    count++;
+                }
+                int result = 0;
+                if (count > 0)
+                    result = rate / count;
+                Dict.Add( new KeyValuePair<product, int>(product, result));
+            }
+            Dict = Dict.OrderBy(p => p.Value).Take(10).ToList();
+            List<product> _product = new List<product>();
+            foreach(KeyValuePair<product, int> item in Dict)
+            {
+                _product.Add(item.Key);
+            }
+            ViewBag.ratedproduct = _product;
             return View();
         }
 
